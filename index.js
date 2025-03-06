ad2e8d27c957f17ea06241992ce5bd4071c23f0e
Donald-27 <+>
2025-03-06 17:30:29 +0300

    modified index.js and .results.json

--- index.js
+++ index.js
@@ -1,11 +1,83 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+// Function to determine the greeting based on ride distance
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return 'This one is on me!';
+  } else if (distance > 400 && distance <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (distance > 2000 && distance <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+// Function to check the city using a ternary operator
+// Function to determine the greeting based on ride distance
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return 'This one is on me!';
+  } else if (distance > 400 && distance <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (distance > 2000 && distance <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+// Function to check the city using a ternary operator
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
+
+// Function to return a response based on the tip given
+// Function to determine the greeting based on ride distance
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return 'This one is on me!';
+  } else if (distance > 400 && distance <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (distance > 2000 && distance <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
+}
+
+// Function to check the city using a ternary operator
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
+
+// Function to return a response based on the tip given
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+}
+
+// Exporting the functions for use in tests
+module.exports = { scuberGreetingForFeet, ternaryCheckCity, switchOnCharmFromTip };
+
+// Exporting the functions for use in tests
+module.exports = { scuberGreetingForFeet, ternaryCheckCity, switchOnCharmFromTip };
+
+// Function to return a response based on the tip given
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+}
+
+// Exporting the functions for use in tests
+module.exports = { scuberGreetingForFeet, ternaryCheckCity, switchOnCharmFromTip };
